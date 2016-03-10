496cbe6dba6f18f75da35c1aa061512de89cc440
James Page <james.page@ubuntu.com>
2016-03-10 10:23:06 +0000

    Re-initialize repositor

---
+++ requires.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from charms.reactive import RelationBase
+from charms.reactive import hook
+from charms.reactive import scopes
+from charmhelpers.core import hookenv
+
+
+class RabbitMQRequires(RelationBase):
+    scope = scopes.GLOBAL
+
+    # These remote data fields will be automatically mapped to accessors
+    # with a basic documentation string provided.
+    auto_accessors = ['password', 'private-address', 'ssl_port',
+                      'ssl_ca', 'ha_queues', 'ha-vip-only', 'clustered', 'vip']
+
+    def vhost(self):
+        return self.get_local('vhost')
+
+    def username(self):
+        return self.get_local('username')
+
+    @hook('{requires:rabbitmq}-relation-joined')
+    def joined(self):
+        self.set_state('{relation_name}.connected')
+
+    def update_state(self):
+        if self.base_data_complete():
+            self.set_state('{relation_name}.available')
+            if self.ssl_data_complete():
+                self.set_state('{relation_name}.available.ssl')
+            else:
+                self.remove_state('{relation_name}.available.ssl')
+        else:
+            self.remove_state('{relation_name}.available')
+            self.remove_state('{relation_name}.available.ssl')
+        if not self.rabbitmq_hosts():
+            self.remove_state('{relation_name}.connected')
+
+    @hook('{requires:rabbitmq}-relation-changed')
+    def changed(self):
+        self.update_state()
+
+    @hook('{requires:rabbitmq}-relation-{broken,departed}')
+    def departed(self):
+        self.update_state()
+
+    def base_data_complete(self):
+        """
+        Get the connection string, if available, or None.
+        """
+        data = {
+            'hostname': self.private_address(),
+            'vhost': self.vhost(),
+            'username': self.username(),
+            'password': self.password(),
+        }
+        if all(data.values()):
+            return True
+        return False
+
+    def ssl_data_complete(self):
+        """
+        Get the connection string, if available, or None.
+        """
+        data = {
+            'ssl_port': self.ssl_port(),
+            'ssl_ca': self.ssl_ca(),
+        }
+        if all(data.values()):
+            return True
+        return False
+
+    def request_access(self, username, vhost):
+        """
+        Request access to vhost for the supplied username.
+        """
+        relation_info = {
+            'username': username,
+            'vhost': vhost,
+        }
+        self.set_local(**relation_info)
+        self.set_remote(**relation_info)
+
+    def configure(self, username, vhost):
+        """
+        DEPRECATED: use request_access instead
+        Request access to vhost for the supplied username.
+        """
+        self.request_access(username, vhost)
+
+    def get_remote_all(self, key, default=None):
+        '''Return a list of all values presented by remote units for key'''
+        values = []
+        for conversation in self.conversations():
+            for relation_id in conversation.relation_ids:
+                for unit in hookenv.related_units(relation_id):
+                    value = hookenv.relation_get(key,
+                                                 unit,
+                                                 relation_id) or default
+                    if value:
+                        values.append(value)
+        return list(set(values))
+
+    def rabbitmq_hosts(self):
+        return self.get_remote_all('private-address')
